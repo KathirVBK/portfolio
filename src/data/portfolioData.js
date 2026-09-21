0038d53e1e29e3ea35791ec7b95ea9cea6aad600
kathirvb24@gmail.com <kathirvb24@gmail.com>
2026-09-21 15:58:47 +0530

    Added quizify

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,6 +1,7 @@
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import profileImg from "../assets/profile.png";
-import movieImg from "../assets/movie.png"; 
+import movieImg from "../assets/movie.png";
+import quiz from "../assets/quiz.png"; 
 import agriSenseImg from "../assets/agriSense.png";
 import attentionBlogImg from "../assets/attention_blog.jpg";
 import Agents from "../assets/Agents.png";
@@ -95,6 +96,17 @@ export const portfolioData = {
     },
       {
       id: 3,
+      title: "Quizify",
+      description:
+        "An AI-powered platform that automatically generates, grades, and provides feedback on custom quizzes from any topic, text, or PDF.",
+      techStack: ["React", "FastAPI", "CrewAI", "Python"],
+      github: "https://github.com/KathirVBK/movie-search-frontend",
+      demo: "https://quiz-1-f2ri.onrender.com",
+      image: quiz,
+      category: "AI Web App / EdTech",
+    },
+    {
+      id: 4 ,
       title: "Movie Search Platform",
       description:
         "A simple frontend project on  movie search platform using React and CSS.",
